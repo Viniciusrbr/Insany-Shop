a093dd732f4e56ef50e1677abbcb17159e9f4cf1
viniciusrbr <viniciusgoes159@gmail.com>
2025-09-03 21:52:05 -0300

    feat: exibindo informações de CategoriesArea com dados da API.

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -25,7 +25,7 @@ export default async function Home({ searchParams }: Props) {
         category={category}
         categories={categories}
       />
-      <CategoriesArea />
+      <CategoriesArea categories={categories} />
     </>
   )
 }
